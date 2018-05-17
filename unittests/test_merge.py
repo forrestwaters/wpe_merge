272159ecd6475ed6fbb916b0d9c35e65c22df169
Forrest <forrest.waters@wpengine.com>
2018-05-16 21:21:26 -0500

    one last working test

--- unittests/test_merge.py
+++ unittests/test_merge.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import unittest
+import tempfile
 sys.path.append('/Users/forrest.waters/projects/scratch/scratch/forrestwaters/wpe_merge') #this seems bad
 from merge import WpeMerge
 from collections import OrderedDict
@@ -8,6 +9,7 @@ from collections import OrderedDict
 
 class TestWpeMerge(unittest.TestCase):
 
+    out_file = 'unittests/new.csv'
     wpm = WpeMerge('unittests/test.csv', 'unittests/new.csv')
 
     def test_parse_csv(self):
@@ -29,10 +31,27 @@ class TestWpeMerge(unittest.TestCase):
     
     def test_merge(self):
         expected_merge = [OrderedDict([('Account ID', '12345'), ('Account Name', 'lexcorp'), ('First Name', 'Lex'), ('Created On', '1/12/11'), ('Status', 'good'), ('Status Set On', '2011-01-12')])]
+        
         self.assertEqual(
             self.wpm.merge(), 
             expected_merge
         )
+
+    
+    def test_write_new_file(self):
+        expected_csv = 'Account ID,First Name,Created On,Status,Status Set On\n12345,Lex,1/12/11,good,2011-01-12\n'
+
+        self.wpm.write_to_new_file()
+        test_csv_file = open(self.out_file, 'r')
+        content = test_csv_file.read()
+        test_csv_file.close()
+
+        self.assertEqual(
+            content,
+            expected_csv
+        )
+
+        os.remove('unittests/new.csv')
 
 
 if __name__ == '__main__':
