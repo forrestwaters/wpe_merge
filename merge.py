4184fe299175c3cf85a763df2db07b73730deca9
Forrest <forrest.waters@wpengine.com>
2018-05-17 06:32:35 -0500

    adding some notes

--- merge.py
+++ merge.py
@@ -6,15 +6,30 @@ API_ENDPOINT = "http://interview.wpengine.io/v1/accounts/"
 
 class WpeMerge(object):
     """
+    Given a csv file with the following columns:
+    Account ID,Account Name,First Name,Created On
+
+    Fetch an api that will have the following fields:
+    Account ID,Status, Status Set On
+
+    Merge this data to a new csv with the following fields:
     Account ID, First Name, Created On, Status, Status Set On
     """
 
     def __init__(self, input_file, output_file):
+        """
+        :param input_file: csv_file to read initial data from.
+        :parap output_file: file that we will write our merged data to
+        """
         self.input_file = input_file
         self.output_file = output_file
 
     
     def parse_csv(self):
+        """
+        Parse the csv as an OrderedDict.
+        :return: list of dicts for each account.
+        """
         with open(self.input_file) as csv_file:
             reader = csv.DictReader(csv_file)
             self.accounts = [row for row in reader]
@@ -22,11 +37,19 @@ class WpeMerge(object):
 
     
     def fetch_api(self, account_id):
+        """
+        :param: pass an account_id
+        :return: json (dict) response from the api
+        """
         with requests.get(API_ENDPOINT + account_id) as x:
             return x.json()
     
 
     def merge(self):
+        """
+        Parse the csv, for each account id hit the API and merge the data
+        :return: list of dictionaries for each account id
+        """
         self.parse_csv()
         self.merged_list = []
         for entry in self.accounts:
@@ -38,6 +61,9 @@ class WpeMerge(object):
 
     
     def write_to_new_file(self):
+        """
+        Call merge() and write the merged data to our output_file
+        """
         self.merge()
         with open(self.output_file, 'w') as csvfile:
             fieldnames = "Account ID", "First Name", "Created On", "Status", "Status Set On"
